4c0cf616ce81c8a5ca0eda7a633a9e7778e20cdf
Elias Ferede <feredeelias@gmail.com>
2020-09-10 14:47:45 -0700

    Updates

---
+++ precache-manifest.b6c9cffa89f7ee8fcaac61aae68e50d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57483150731dff12be244cc7d5b81428",
+    "url": "/Javascript-Calculator/index.html"
+  },
+  {
+    "revision": "4704a323f8a2cf179960",
+    "url": "/Javascript-Calculator/static/css/main.19159e11.chunk.css"
+  },
+  {
+    "revision": "27b48ac9bb255ca80be2",
+    "url": "/Javascript-Calculator/static/js/2.2b329f98.chunk.js"
+  },
+  {
+    "revision": "4704a323f8a2cf179960",
+    "url": "/Javascript-Calculator/static/js/main.719de26f.chunk.js"
+  },
+  {
+    "revision": "d58731c9228ee059f562",
+    "url": "/Javascript-Calculator/static/js/runtime~main.e1b29566.js"
+  }
+]);
