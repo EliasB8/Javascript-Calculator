3e01009a906bfc733570bce9bb8a9b9502002494
Elias Ferede <feredeelias@gmail.com>
2020-09-14 13:44:07 -0700

    title

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Javascript-Calcualtor/precache-manifest.16f0967ba6fff245a9d65bd1707df9ba.js"
+  "/Javascript-Calculator/precache-manifest.b6c9cffa89f7ee8fcaac61aae68e50d5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Javascript-Calcualtor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Javascript-Calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
